3ef29eca91204753fa940d900ee99d81424e83f7
aravinda <aravinda1879@colinawork.chem.ufl.edu>
2019-04-05 10:40:44 -0400

    Initial commit after CMS Cookiecutter creation, version 1.0

---
+++ molssi_math/__init__.py
@@ -0,0 +1,14 @@
+"""
+molssi_math
+molssi_bestpractices
+"""
+
+# Add imports here
+from .molssi_math import *
+
+# Handle versioneer
+from ._version import get_versions
+versions = get_versions()
+__version__ = versions['version']
+__git_revision__ = versions['full-revisionid']
+del get_versions, versions
